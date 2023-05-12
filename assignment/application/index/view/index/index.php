928512549fa67f6e297591b32b35800cb6db7f01
cyh <2608577T@student.gla.ac.uk>
2023-05-12 09:40:20 +0800

    'code'

---
+++ assignment/application/index/view/index/index.php
@@ -0,0 +1,212 @@
+﻿
+<!DOCTYPE html>
+    <html lang="en">
+
+    <head>
+        <meta charset="utf-8" />
+        <title>Coca-Cola</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta name="description" content="Premium Bootstrap 4 Landing Page Template" />
+        <meta name="keywords" content="bootstrap 4, premium, clen, modern, landing, multipurpose" />
+        <meta content="Themesdesign" name="author" />
+        <!-- favicon -->
+        <!-- Magnific -->
+        <link href="__STATIC__/index/css/magnific-popup.css" rel="stylesheet" type="text/css" />
+        <!-- css -->
+        <link href="__STATIC__/index/css/bootstrap.min.css" rel="stylesheet" type="text/css" />
+        <link href="__STATIC__/index/css/materialdesignicons.min.css" rel="stylesheet" type="text/css" />
+        <link href="__STATIC__/index/css/style.css" rel="stylesheet" type="text/css" />
+        <!-- Slider -->
+        <link rel="stylesheet" href="__STATIC__/index/css/slick-theme.css"/>
+        <link rel="stylesheet" href="__STATIC__/index/css/slick.css"/>
+        <link rel='stylesheet' type='text/css' href='https://www.x3dom.org/download/x3dom.css'/>
+        <script type='text/javascript' src='https://www.x3dom.org/download/x3dom.js'> </script>
+    </head>
+
+    <body>
+        <!-- Navbar Start -->
+        <nav class="navbar navbar-expand-lg fixed-top navbar-custom navbar-light sticky">
+    		<div class="container">
+			    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
+			        <span class="mdi mdi-menu"></span>
+			    </button><!--end button-->
+
+			    <div class="collapse navbar-collapse" id="navbarCollapse">
+			        <ul class="navbar-nav ml-auto">
+			            <li class="nav-item active">
+                            <a class="nav-link" href="#home">Home</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#about">about</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#Contact">Contact</a>
+                        </li>
+
+                    </ul><!--end navbar nav-->
+			    </div><!--end collapse-->
+		    </div><!--end container-->
+		</nav><!--end navbar-->
+        <!-- Navbar End -->
+
+        <!-- Hero Start -->
+        <section class="bg-home" style="background-image:url('__STATIC__/index/images/home/img.png')" id="home">
+            <div class="bg-overlay"></div>
+            <div class="home-center">
+                <div class="home-desc-center">
+                    <div class="container">
+                        <div class="row justify-content-center">
+                            <div class="col-lg-8">
+                                <div class="title-heading text-center mt-5 pt-4">
+                                    <h1 class="heading text-white mb-3">HOLIDAYS ARE COMING!
+                                    </h1>
+                                    <p class="para-desc mx-auto text-light">This year, the eagerly anticipated Coca-Cola Christmas Truck Tour will return, aiming to bring the magic of Christmas to people nationwide.</p>
+                                    <div class="mt-4 pt-2">
+                                        <a href="#model" class="btn btn-custom">View product model</a>
+                                    </div>
+                                </div>
+                            </div><!--end col-->
+                        </div><!--end row-->
+                    </div><!--end container-->
+                </div><!--end home desc center-->
+            </div><!--end home center-->
+        </section><!--end section-->
+        <!-- Hero End -->
+
+
+
+        <!-- Services Start -->
+        <section class="section bg-light" id="about">
+            <div class="container">
+                <div class="row justify-content-center">
+                    <div class="col-12 text-center">
+                        <div class="section-title">
+                            <h4 class="title text-uppercase mb-4">about the website ?</h4>
+                            <p class="text-muted mx-auto para-desc mb-0">I created this website by imitating the official website of Coca Cola. I used some UI libraries and js libraries, which can save my development time and also make more beautiful products.</p>
+                        </div>
+                    </div><!--end col-->
+                </div><!--end row-->
+
+
+            </div><!--end container-->
+        </section><!--end section-->
+        <!-- Services End -->
+
+
+
+        <!-- Team Start -->
+        <section class="section" id="Contact">
+            <div class="container">
+                <div class="row justify-content-center">
+                    <div class="col-12 text-center">
+                        <div class="section-title">
+                            <h4 class="title text-uppercase mb-4">CONTACT</h4>
+                            <p class="text-muted mx-auto para-desc mb-0">I'm a junior majoring in computer science from Sussex, and my contact email is hw480@sussex.ac.uk.</p>
+                        </div>
+                    </div><!--end col-->
+                </div><!--end row-->
+
+
+            </div><!--end container-->
+        </section><!--end section-->
+        <!-- Team End -->
+
+
+        <!-- Project End -->
+
+
+
+
+
+        <!-- Blog Start -->
+        <section class="section" id="model">
+            <div class="container">
+                <div class="row justify-content-center">
+                    <div class="col-12 text-center">
+                        <div class="section-title">
+                            <h4 class="title text-uppercase mb-4">models</h4>
+                            <p class="text-muted mx-auto para-desc mb-0">Here are the 3D models of the three latest products.</p>
+                        </div>
+                    </div><!--end col-->
+                </div><!--end row-->
+
+                <div class="row">
+                    <div class="col-lg-4 col-md-6 col-12 mt-4 pt-2">
+                        <div class="blog-post rounded bg-light">
+
+                            <x3d  class="img-fluid rounded-top" style="width: 300px;height: 300px" scale="0.8 0.8 0.8" translation="0 0.4 0">
+
+                                    <scene>
+                                        <Transform scale="2.0 2.0 2.0" translation="0 0 0" rotation='0 0 0 0'>
+                                            <inline url="__STATIC__/index/model/model1.x3d"> </inline>
+                                        </Transform>
+                                    </scene>
+
+                            </x3d>
+                            <div class="content pt-4 pb-4 p-3">
+                                <a href="javascript:void(0)"><h5 class="title text-dark">Bright, bubbly, instantly refreshing and great tasting. Fanta is made with 100% natural flavours and is caffeine free. Fanta is available in a variety of real fruit flavours.</h5></a>
+                            </div><!--end content-->
+                        </div><!--end blog post-->
+                    </div><!--end col-->
+
+                    <div class="col-lg-4 col-md-6 col-12 mt-4 pt-2">
+                        <div class="blog-post rounded bg-light">
+                            <x3d  class="img-fluid rounded-top" style="width: 300px;height: 300px" scale="0.8 0.8 0.8" translation="0 0.4 0">
+
+                                <scene>
+                                    <Transform scale="2.0 2.0 2.0" translation="0 0 0" rotation='0 0 0 0'>
+                                        <inline url="__STATIC__/index/model/model2.x3d"> </inline>
+                                    </Transform>
+                                </scene>
+
+                            </x3d>
+                            <div class="content pt-4 pb-4 p-3">
+                                <a href="javascript:void(0)"><h5 class="title text-dark">Served in London since 1971, Costa Coffee's signature blend is the perfect balance of delicate Arabica and strong Robusta beans, slow-roasted for a smooth and nutty flavour and rich aroma. Now you can experience it in a can on the go.</h5></a>
+                            </div><!--end content-->
+                        </div><!--end blog post-->
+                    </div><!--end col-->
+
+                    <div class="col-lg-4 col-md-6 col-12 mt-4 pt-2">
+                        <div class="blog-post rounded bg-light">
+                            <x3d  class="img-fluid rounded-top" style="width: 300px;height: 300px" scale="0.8 0.8 0.8" translation="0 0.4 0">
+
+                                <scene>
+                                    <Transform scale="2.0 2.0 2.0" translation="0 0 0" rotation='0 0 0 0'>
+                                        <inline url="__STATIC__/index/model/model3.x3d"> </inline>
+                                    </Transform>
+                                </scene>
+
+                            </x3d>
+                            <div class="content pt-4 pb-4 p-3">
+                                <a href="javascript:void(0)"><h5 class="title text-dark">Feel the fabulous fizz of Schweppes Classic: delicately balanced mixers sparkling with brilliant bubbles. Made for mixing - or simply to quench your thirst.</h5></a>
+                            </div><!--end content-->
+                        </div><!--end blog post-->
+                    </div><!--end col-->
+                </div><!--end row-->
+            </div><!--end container-->
+        </section><!--end section-->
+        <!-- Blog End -->
+
+
+
+        <!-- javascript -->
+        <script src="__STATIC__/index/js/jquery-3.5.1.min.js"></script>
+        <script src="__STATIC__/index/js/bootstrap.bundle.min.js"></script>
+        <script src="__STATIC__/index/js/jquery.easing.min.js"></script>
+        <script src="__STATIC__/index/js/scrollspy.min.js"></script>
+        <script src="__STATIC__/index/js/menu.js"></script>
+        <!-- Magnific Popup -->
+        <script src="__STATIC__/index/js/jquery.magnific-popup.min.js"></script>
+        <script src="__STATIC__/index/js/isotope.js"></script>
+        <script src="__STATIC__/index/js/portfolio.init.js"></script>
+        <script src="__STATIC__/index/js/magnific.init.js"></script>
+        <!-- Slider -->
+        <script src="__STATIC__/index/js/slick.min.js "></script>
+        <!-- Contact -->
+        <script src="__STATIC__/index/js/contact.js"></script>
+        <!-- Main Js -->
+        <script src="__STATIC__/index/js/app.js"></script>
+
+    </body>
+
+</html>
